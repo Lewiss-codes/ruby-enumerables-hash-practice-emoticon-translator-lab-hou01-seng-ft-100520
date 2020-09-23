3bb7d9e480a6f08d65ac0eef55bc1db96d328ad3
Luis Penaloza <ldp231@yahoo.com>
2020-09-23 18:23:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,13 @@
 # require modules here
-#require 'ymal'
+require 'ymal'
 
-def load_library(arg)
+def load_library(file_path)
   # code goes here
-  emoticon = yaml.load_file(arg)
-  result = emo_library.each_with_object({}) do |(key, value), final_hash| 
+  emoticon = YAML.load_file(file_path)
+  result = emoticon.each_with_object({}) do |(key, value), final_hash| 
     if !final_hash[key] 
       final_hash[key] = { 
-        :english => value[0] 
+        :english => value[0],
         :japanese => value[1] 
       } 
     end 
