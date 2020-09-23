60007a0348542c6cb3906ec97b2b9b6b63864d78
Luis Penaloza <ldp231@yahoo.com>
2020-09-23 18:09:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,14 +5,7 @@ require 'ymal'
 def load_library(file_emoticon)
   # code goes here
   emo_library = YAML.load_file(file_emoticon)
-  result = emo_library.each_with_object({}) do |(key, value), final_hash|
-    if !final_hash[key]
-      final_hash[key] = {
-        :english => value[0]
-        :japanese => value[1]
-      }
-    end
-    result
+  
   end
 end
 
