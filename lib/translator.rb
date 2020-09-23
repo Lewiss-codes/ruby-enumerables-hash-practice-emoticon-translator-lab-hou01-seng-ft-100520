c2fb06f21035a7dbae67e91692d48683a2a3924d
Luis Penaloza <ldp231@yahoo.com>
2020-09-23 18:20:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,14 @@
 def load_library(arg)
   # code goes here
   emoticon = yaml.load_file(arg)
-  
+  result = emo_library.each_with_object({}) do |(key, value), final_hash| 
+    if !final_hash[key] 
+      final_hash[key] = { 
+        :english => value[0] 
+        :japanese => value[1] 
+      } 
+    end 
+    result 
   end
 end
 
