e5a68040387008f887581ac9fc07c19c4cedb884
Luis Penaloza <ldp231@yahoo.com>
2020-09-23 18:19:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
 
 def load_library(arg)
   # code goes here
-  emo_library = ymal.load_file(arg)
+  emoticon = YAML.load_file(arg)
   hash.each do |element|
     print "#{element[0]}"
     print "#{element[1][0]}"
